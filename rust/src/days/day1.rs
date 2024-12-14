e610149065f8e6a90c5517025cddad9e0c74b057
Nelson Earle <nelson.earle137@gmail.com>
2024-12-14 01:44:33 -0600

    feat(day1): solve part 1

--- rust/src/days/day1.rs
+++ rust/src/days/day1.rs
@@ -1,11 +1,33 @@
 pub(super) fn part1(input: &str) -> Box<dyn std::fmt::Display> {
-    println!("Part 1 | {}", input.len());
+    let mut left = Vec::<u32>::new();
+    let mut right = Vec::<u32>::new();
 
-    Box::new("_")
+    for line in input.lines() {
+        let l = line[0..5].parse::<u32>().unwrap();
+        let r = line[8..13].parse::<u32>().unwrap();
+
+        left.push(l);
+        right.push(r);
+    }
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let answer = left
+        .into_iter()
+        .zip(right)
+        .map(|(l, r)| {
+            let min = l.min(r);
+            let max = l.max(r);
+            max - min
+        })
+        .sum::<u32>();
+
+    Box::new(answer)
 }
 
 pub(super) fn part2(input: &str) -> Box<dyn std::fmt::Display> {
-    println!("Part 2 | {}", input.len());
+    _ = input;
 
     Box::new("_")
 }
