a03059ed4ea24b8963a5e5ccaf8fbbd9a5a4f6e2
Nelson Earle <nelson.earle137@gmail.com>
2024-12-14 12:14:17 -0600

    feat(day3): solve part 2

--- rust/src/days/day3.rs
+++ rust/src/days/day3.rs
@@ -16,7 +16,25 @@ pub(super) fn part1(input: &str) -> Box<dyn std::fmt::Display> {
 }
 
 pub(super) fn part2(input: &str) -> Box<dyn std::fmt::Display> {
-    _ = input;
+    let args_re =
+        Regex::new(r"(?<do>do\(\))|(?<dont>don't\(\))|(mul\((?<a>\d+),(?<b>\d+)\))").unwrap();
 
-    Box::new("_")
+    let mut enabled = true;
+    let mut sum = 0;
+
+    for cap in args_re.captures_iter(input) {
+        if cap.name("do").is_some() {
+            enabled = true;
+        } else if cap.name("dont").is_some() {
+            enabled = false;
+        } else if enabled {
+            if let (Some(a), Some(b)) = (cap.name("a"), cap.name("b")) {
+                let a = a.as_str().parse::<u32>().unwrap();
+                let b = b.as_str().parse::<u32>().unwrap();
+                sum += a * b;
+            }
+        }
+    }
+
+    Box::new(sum)
 }
