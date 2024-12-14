0d182fe1597c49902f12a8f76205aacfeb412fae
Nelson Earle <nelson.earle137@gmail.com>
2024-12-14 16:31:01 -0600

    r(day5): improve data ownership

--- rust/src/days/day5.rs
+++ rust/src/days/day5.rs
@@ -1,34 +1,37 @@
 use std::{cmp::Ordering, iter};
 
-struct PrintUpdateOrganizer {
-    max_page: usize,
-    rules_adjacent: Vec<bool>,
-    updates: Vec<Vec<u32>>,
+fn parse(input: &str) -> (PrintRules, Vec<Vec<u32>>) {
+    let mut rules = Vec::<(u32, u32)>::new();
+    let mut updates = Vec::<Vec<u32>>::new();
+
+    let mut parsing_rules = true;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            parsing_rules = false;
+            continue;
+        }
+        if parsing_rules {
+            let (a, b) = line.split_once('|').unwrap();
+            let a = a.parse::<u32>().unwrap();
+            let b = b.parse::<u32>().unwrap();
+            rules.push((a, b));
+        } else {
+            let update = line.split(',').map(|u| u.parse::<u32>().unwrap()).collect();
+            updates.push(update);
+        }
+    }
+
+    (PrintRules::new(rules), updates)
 }
 
-impl PrintUpdateOrganizer {
-    fn new(input: &str) -> Self {
-        let mut rules = Vec::<(u32, u32)>::new();
-        let mut updates = Vec::<Vec<u32>>::new();
+struct PrintRules {
+    max_page: usize,
+    rules_adjacent: Vec<bool>,
+}
 
-        let mut parsing_rules = true;
-
-        for line in input.lines() {
-            if line.is_empty() {
-                parsing_rules = false;
-                continue;
-            }
-            if parsing_rules {
-                let (a, b) = line.split_once('|').unwrap();
-                let a = a.parse::<u32>().unwrap();
-                let b = b.parse::<u32>().unwrap();
-                rules.push((a, b));
-            } else {
-                let update = line.split(',').map(|u| u.parse::<u32>().unwrap()).collect();
-                updates.push(update);
-            }
-        }
-
+impl PrintRules {
+    fn new(rules: Vec<(u32, u32)>) -> Self {
         let max_page = rules
             .iter()
             .copied()
@@ -46,35 +49,33 @@ impl PrintUpdateOrganizer {
         Self {
             max_page,
             rules_adjacent,
-            updates,
         }
     }
-}
 
-fn update_is_valid(update: &[u32], rules_adjacent: &[bool], max_page: usize) -> bool {
-    for (i, u) in update.iter().copied().enumerate().rev() {
-        for &prev in &update[..i] {
-            if rules_adjacent[u as usize * max_page + prev as usize] {
-                return false;
+    fn update_is_valid(&self, update: &[u32]) -> bool {
+        for (i, u) in update.iter().copied().enumerate().rev() {
+            for &prev in &update[..i] {
+                if self.rules_adjacent[u as usize * self.max_page + prev as usize] {
+                    return false;
+                }
             }
         }
+        true
     }
-    true
+
+    fn adjacent(&self, a: u32, b: u32) -> bool {
+        let a = a as usize;
+        let b = b as usize;
+        self.rules_adjacent[a * self.max_page + b]
+    }
 }
 
 pub(super) fn part1(input: &str) -> Box<dyn std::fmt::Display> {
-    let print_organizer = PrintUpdateOrganizer::new(input);
+    let (print_rules, updates) = parse(input);
 
-    let answer = print_organizer
-        .updates
+    let answer = updates
         .iter()
-        .filter(|update| {
-            update_is_valid(
-                update,
-                &print_organizer.rules_adjacent,
-                print_organizer.max_page,
-            )
-        })
+        .filter(|update| print_rules.update_is_valid(update))
         .map(|update| update[update.len() / 2])
         .sum::<u32>();
 
@@ -82,23 +83,14 @@ pub(super) fn part1(input: &str) -> Box<dyn std::fmt::Display> {
 }
 
 pub(super) fn part2(input: &str) -> Box<dyn std::fmt::Display> {
-    let mut print_organizer = PrintUpdateOrganizer::new(input);
+    let (print_rules, mut updates) = parse(input);
 
-    let answer = print_organizer
-        .updates
+    let answer = updates
         .iter_mut()
-        .filter(|update| {
-            !update_is_valid(
-                update,
-                &print_organizer.rules_adjacent,
-                print_organizer.max_page,
-            )
-        })
+        .filter(|update| !print_rules.update_is_valid(update))
         .map(|update| {
             update.sort_by(|&a, &b| {
-                if print_organizer.rules_adjacent
-                    [a as usize * print_organizer.max_page + b as usize]
-                {
+                if print_rules.adjacent(a, b) {
                     Ordering::Less
                 } else {
                     Ordering::Greater
