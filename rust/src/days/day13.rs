10a882012346e5856de14024f60b66d144e012cc
Nelson Earle <nelson.earle137@gmail.com>
2025-01-13 19:23:57 -0600

    feat(day13): optimize part 1 (#1)

--- rust/src/days/day13.rs
+++ rust/src/days/day13.rs
@@ -37,21 +37,73 @@ impl ClawMachine {
         }
     }
 
+    /// Solve for the variables `A` and `B` in the Claw Machine Equations.
+    ///
+    /// Return the number of tokens required to play the Claw Machine and
+    /// position the claw over the prize (`3*A + B`) if a solution exists.
+    ///
+    /// Equations:
+    ///
+    /// For some given Claw Machine Button vectors `(a_x, a_y)` and
+    /// `(b_x, b_y)` and a Prize vector `(p_x, p_y)` there may exist some
+    /// combination of variables `A` and `B` such that `A*a_x + B*b_x = p_x`
+    /// (Eq1) and `A*a_y + B*b_y = p_y` (Eq2).
+    ///
+    /// With these equations, we can isolate `A` and `B` to create a
+    /// constant-time algorithm for computing the values of `A` and `B`.
+    ///
+    /// Isolate `B` (using Eq2):
+    ///
+    /// ```
+    /// A*a_y + B*b_y = p_y
+    /// B*b_y = p_y - A*a_y
+    /// B = (p_y - A*a_y) / b_y
+    /// ```
+    ///
+    /// Solve for `A` (using Eq1):
+    ///
+    /// ```
+    /// A*a_x + B*b_x = p_x
+    /// A*a_x + b_x*(p_y - A*a_y)/b_y = p_x
+    /// A*a_x + b_x*p_y/b_y - b_x*A*a_y/b_y = p_x
+    /// A*(a_x - b_x*a_y/b_y) = p_x - b_x*p_y/b_y
+    /// A = (p_x - b_x*p_y/b_y)/(a_x - b_x*a_y/b_y)
+    /// ```
+    ///
+    /// For the solution to be valid
     fn solve(&self) -> Option<u32> {
         let (a_x, a_y) = self.a.into();
         let (b_x, b_y) = self.b.into();
         let (p_x, p_y) = self.prize.into();
 
-        for a in 1..=100 {
-            for b in 1..=100 {
-                let x = a * a_x + b * b_x;
-                let y = a * a_y + b * b_y;
-                if (x, y) == (p_x, p_y) {
-                    return Some(3 * a + b);
-                }
-            }
-        }
+        let a = (p_x - b_x * p_y / b_y) / (a_x - b_x * a_y / b_y);
+        let b = (p_y - a * a_y) / b_y;
 
+        match (a.to_whole_number(), b.to_whole_number()) {
+            (Some(a), Some(b)) => Some(3 * a + b),
+            _ => None,
+        }
+    }
+}
+
+trait FloatToWholeNumber {
+    type Target;
+    fn to_whole_number(self) -> Option<Self::Target>;
+}
+
+impl FloatToWholeNumber for f32 {
+    type Target = u32;
+    #[inline(always)]
+    fn to_whole_number(self) -> Option<Self::Target> {
+        const SOLUTION_EPSILON: f32 = 1000. * f32::EPSILON;
+        let mut f = self.fract();
+        if f < SOLUTION_EPSILON {
+            return Some(self as u32);
+        }
+        f = 1. - f;
+        if f < SOLUTION_EPSILON {
+            return Some(self as u32 + 1);
+        }
         None
     }
 }
@@ -59,7 +111,7 @@ impl ClawMachine {
 #[derive(Clone, Copy)]
 struct Button(Vec2);
 
-impl From<Button> for (u32, u32) {
+impl From<Button> for (f32, f32) {
     #[inline(always)]
     fn from(val: Button) -> Self {
         val.0.into()
@@ -69,7 +121,7 @@ impl From<Button> for (u32, u32) {
 #[derive(Clone, Copy)]
 struct Prize(Vec2);
 
-impl From<Prize> for (u32, u32) {
+impl From<Prize> for (f32, f32) {
     #[inline(always)]
     fn from(val: Prize) -> Self {
         val.0.into()
@@ -78,23 +130,23 @@ impl From<Prize> for (u32, u32) {
 
 #[derive(Clone, Copy)]
 struct Vec2 {
-    x: u32,
-    y: u32,
+    x: f32,
+    y: f32,
 }
 
 impl Vec2 {
     #[inline(always)]
-    fn new(x: u32, y: u32) -> Self {
+    fn new(x: f32, y: f32) -> Self {
         Self { x, y }
     }
 }
 
 #[inline(always)]
-fn vec2(x: u32, y: u32) -> Vec2 {
+fn vec2(x: f32, y: f32) -> Vec2 {
     Vec2::new(x, y)
 }
 
-impl From<Vec2> for (u32, u32) {
+impl From<Vec2> for (f32, f32) {
     #[inline(always)]
     fn from(val: Vec2) -> Self {
         (val.x, val.y)
@@ -162,4 +214,27 @@ fn part2(input: &str) -> Option<Box<dyn std::fmt::Display>> {
     _ = input;
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CASES: &[(Option<u32>, f32)] = &[
+        (Some(0), 0.),
+        (Some(0), 0. + f32::EPSILON),
+        (None, 0.2),
+        (None, 0.8),
+        (Some(1), 1. - f32::EPSILON),
+        (Some(1), 1.),
+        (Some(1), 1. + f32::EPSILON),
+        (None, 1.2),
+    ];
+
+    #[test]
+    fn to_whole_number() {
+        for &(expected, x) in CASES {
+            assert_eq!(expected, x.to_whole_number());
+        }
+    }
 }
