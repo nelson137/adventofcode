3bdb6c157c95a25b5004996d5c573e4e5896fbde
Nelson Earle <nelson.earle137@gmail.com>
2025-12-05 00:10:41 -0600

    feat(day2): optimize part 1

--- rust/src/puzzles/year2025/day2.rs
+++ rust/src/puzzles/year2025/day2.rs
@@ -1,5 +1,7 @@
 use std::fmt::Write;
 
+use adventofcode::count_digits;
+
 inventory::submit!(crate::days::DayModule::new("2025", 2).with_executors(
     crate::day_part_executors![part1_brute],
     crate::day_part_executors![part2],
@@ -22,17 +24,26 @@ fn part1_brute(input: &str) -> Option<Box<dyn std::fmt::Display>> {
     let mut id_string = String::new();
     let mut invalid_id_acc = 0_u64;
 
-    for id_range in parse_product_id_ranges(input) {
-        let start: u64 = id_range.raw_start.parse().unwrap();
-        let end: u64 = id_range.raw_end.parse().unwrap();
+    for ProductIdRange { raw_start, raw_end } in parse_product_id_ranges(input) {
+        let start_len = raw_start.len();
+        let end_len = raw_end.len();
+
+        let is_start_len_even = start_len % 2 == 0;
+        let is_end_len_even = end_len % 2 == 0;
+
+        if !is_start_len_even && !is_end_len_even {
+            continue;
+        }
+
+        let start: u64 = raw_start.parse().unwrap();
+        let end: u64 = raw_end.parse().unwrap();
 
         for id in start..=end {
             id_string.clear();
             write!(&mut id_string, "{id}").unwrap();
-            if id_string.len() % 2 == 0
-                && let (l, r) = id_string.split_at(id_string.len() / 2)
-                && l == r
-            {
+
+            let (l, r) = id_string.split_at(id_string.len() / 2);
+            if l == r {
                 invalid_id_acc += id;
             }
         }
