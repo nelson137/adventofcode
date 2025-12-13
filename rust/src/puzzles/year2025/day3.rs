e5e415a424676ca387c533cfde4520cc8d3bcef7
Nelson Earle <nelson.earle137@gmail.com>
2025-12-13 17:27:55 -0600

    feat(day3): solve part 2

--- rust/src/puzzles/year2025/day3.rs
+++ rust/src/puzzles/year2025/day3.rs
@@ -24,6 +24,27 @@ impl BatteryBank<'_> {
         // Convert to joltage value
         (tens_digit - b'0') as u32 * 10 + (ones_digit - b'0') as u32
     }
+
+    fn max_joltage_high_power(&self) -> u64 {
+        let mut max_joltage = 0;
+        let mut max_digit_j = -1_i32;
+
+        for i in (0..12).rev() {
+            max_digit_j += 1;
+            let mut max_digit = self.0[max_digit_j as usize];
+
+            for j in max_digit_j as usize + 1..self.0.len() - i {
+                if self.0[j] > max_digit {
+                    max_digit = self.0[j];
+                    max_digit_j = j as i32;
+                }
+            }
+
+            max_joltage += (max_digit - b'0') as u64 * 10_u64.pow(i as u32);
+        }
+
+        max_joltage
+    }
 }
 
 fn parse_battery_banks(input: &str) -> impl Iterator<Item = BatteryBank<'_>> {
@@ -41,7 +62,11 @@ fn part1(input: &str) -> Option<Box<dyn std::fmt::Display>> {
 }
 
 fn part2(input: &str) -> Option<Box<dyn std::fmt::Display>> {
-    _ = input;
+    let mut total_joltage = 0_u64;
 
-    None
+    for bank in parse_battery_banks(input) {
+        total_joltage += bank.max_joltage_high_power();
+    }
+
+    Some(Box::new(total_joltage))
 }
