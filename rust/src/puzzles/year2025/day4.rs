577162a0473350da78df14e5b6ff00ba334492b2
Nelson Earle <nelson.earle137@gmail.com>
2025-12-22 18:20:37 -0600

    feat(day4): solve part 2

--- rust/src/puzzles/year2025/day4.rs
+++ rust/src/puzzles/year2025/day4.rs
@@ -1,4 +1,4 @@
-use adventofcode::{Grid2D, GridIndex2D, Pos};
+use adventofcode::{Grid1D, Grid2D, GridIndex1D, GridIndex2D, Pos};
 
 inventory::submit!(crate::days::DayModule::new("2025", 4).with_executors(
     crate::day_part_executors![part1],
@@ -85,8 +85,117 @@ fn part1(input: &str) -> Option<Box<dyn std::fmt::Display>> {
     Some(Box::new(accessible))
 }
 
-fn part2(input: &str) -> Option<Box<dyn std::fmt::Display>> {
-    _ = input;
+struct ForkliftManager {
+    width: usize,
+    height: usize,
+    map: Vec<bool>,
+}
 
-    None
+impl Grid1D for ForkliftManager {
+    type Item = bool;
+
+    fn extent(&self) -> usize {
+        self.width
+    }
+
+    fn grid_inner(&self) -> &[Self::Item] {
+        self.map.as_slice()
+    }
+
+    fn grid_inner_mut(&mut self) -> &mut [Self::Item] {
+        self.map.as_mut_slice()
+    }
+}
+
+impl ForkliftManager {
+    fn from_paper_map(paper_map: &PaperMap) -> Self {
+        let mut map = Vec::with_capacity(paper_map.width * paper_map.map.len());
+        map.extend(
+            paper_map
+                .map
+                .iter()
+                .flat_map(|row| row.iter().copied().map(|b| b == b'@')),
+        );
+
+        Self {
+            width: paper_map.width,
+            height: paper_map.map.len(),
+            map,
+        }
+    }
+
+    fn remove_accesible_rolls(&mut self) -> usize {
+        let mut removed = 0;
+
+        for r in 0..self.height {
+            for c in 0..self.width {
+                let pos = Pos::new(r, c);
+
+                if !*self.index_1d(pos) {
+                    continue;
+                }
+
+                let mut adjacent = 0_u32;
+
+                let has_adj_n = pos.row > 0;
+                let has_adj_e = (pos.col as usize) < self.width - 1;
+                let has_adj_s = (pos.row as usize) < self.height - 1;
+                let has_adj_w = pos.col > 0;
+
+                if has_adj_n && *self.index_1d(pos.nn()) {
+                    adjacent += 1;
+                }
+                if has_adj_n && has_adj_e && *self.index_1d(pos.ne()) {
+                    adjacent += 1;
+                }
+                if has_adj_e && *self.index_1d(pos.ee()) {
+                    adjacent += 1;
+                }
+                if has_adj_s && has_adj_e && *self.index_1d(pos.se()) {
+                    adjacent += 1;
+                }
+                if has_adj_s && *self.index_1d(pos.ss()) {
+                    adjacent += 1;
+                }
+                if has_adj_s && has_adj_w && *self.index_1d(pos.sw()) {
+                    adjacent += 1;
+                }
+                if has_adj_w && *self.index_1d(pos.ww()) {
+                    adjacent += 1;
+                }
+                if has_adj_n && has_adj_w && *self.index_1d(pos.nw()) {
+                    adjacent += 1;
+                }
+                if adjacent < 4 {
+                    *self.index_1d_mut(pos) = false;
+                    removed += 1;
+                }
+            }
+        }
+
+        removed
+    }
+
+    fn remove_all_accesible_rolls(&mut self) -> usize {
+        let mut removed = 0;
+
+        loop {
+            let r = self.remove_accesible_rolls();
+            if r == 0 {
+                break;
+            }
+            removed += r;
+        }
+
+        removed
+    }
+}
+
+fn part2(input: &str) -> Option<Box<dyn std::fmt::Display>> {
+    let map = PaperMap::parse(input);
+    let mut forklift_mngr = ForkliftManager::from_paper_map(&map);
+
+    let moved = forklift_mngr.remove_all_accesible_rolls();
+
+    Some(Box::new(moved))
 }
